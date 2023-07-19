f766fd017643070322d7b553e1959194fa3aa0c1
Valery <11w-walerik@mail.ru>
2023-07-19 14:24:47 +0300

    37 done

--- 01-event-loop-events/index.js
+++ 01-event-loop-events/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const dns = require('dns');
 
 function timestamp() {
 	return performance.now().toFixed(2);
@@ -6,15 +7,30 @@ function timestamp() {
 
 console.log('Program start');
 
-setTimeout(() => console.log('Timeout 1', timestamp()), 0);
-setTimeout(() => console.log('Timeout 2', timestamp()), 10);
+// Close events
+fs.writeFile('./test.txt', 'Hello node', () => console.log('File written', ' - ', timestamp()));
 
-fs.writeFile('./test.txt', 'Hello node', () => console.log('File written', timestamp()));
+// Promises
+Promise.resolve().then(() => console.log('Promise 1', ' - ', timestamp()));
 
-Promise.resolve().then(() => console.log('Promise 1', timestamp()));
+// Next tick
+process.nextTick(() => console.log('next tick 1', ' - ', timestamp()));
 
-process.nextTick(() => console.log('next tick 1', timestamp()));
+// setImmediate (Check)
+setImmediate(() => console.log('Immediate 1', ' - ', timestamp()));
 
-setImmediate(() => console.log('Immediate 1', timestamp()));
+// Timeouts
+setTimeout(() => console.log('Timeout 1', ' - ', timestamp()), 0);
+setTimeout(() => {
+	process.nextTick(() => console.log('Next tick 2', ' - ', timestamp()));
+	console.log('Timeout 2', ' - ', timestamp());
+}, 10);
+
+// I/O events
+dns.lookup('hoster.by', (err, address, family) => {
+	console.log('DNS hoster.by: ', address, ' - ', timestamp());
+	Promise.resolve().then(() => console.log('Promise 2', ' - ', timestamp()));
+	process.nextTick(() => console.log('Next tick 3', ' - ', timestamp()));
+});
 
 console.log('Program end');
