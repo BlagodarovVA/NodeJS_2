a525dd043946837e00126453941c94c643777fe8
Valery <11w-walerik@mail.ru>
2023-07-27 16:49:14 +0300

    57 done

--- 03-commonjs-modules/export-and-import.js
+++ 03-commonjs-modules/export-and-import.js
@@ -1,6 +1,8 @@
-const { myName } = require('./multiple-exports');
+const { myName, myHobbies } = require('./multiple-exports');
 
 const myFriendsname = 'Diana';
 
 module.exports.myName = myName;
 module.exports.myFriendsname = myFriendsname;
+// имя свойства может отличаться от имени переменной
+module.exports.myGreatHobbies = myHobbies;
--- 03-commonjs-modules/index.js
+++ 03-commonjs-modules/index.js
@@ -1,8 +1,13 @@
 const { myName, myHobbies, myFavouriteNumber } = require('./multiple-exports');
+
 // не нужно использовать абсолютные пути
 // const greetingFn = require('d:\\JS\\Node_2\\03-commonjs-modules\\my-modules\\single-export.js');
 const greetingFn = require('./my-modules/single-export');
-const { myName: myOtherName, myFriendsname } = require('./export-and-import');
+const {
+	myName: myOtherName,
+	myFriendsname,
+	myGreatHobbies,
+} = require('./export-and-import');
 
 // console.log(arguments.callee.toString());
 
@@ -13,11 +18,18 @@ console.log(__filename);
 // console.log(require);
 
 console.log('---------------------------------');
+
+// импорты multiple-exports
 console.log(myName);
 console.log(myHobbies);
 console.log(myFavouriteNumber);
 
+myHobbies.push('juorney');
+
+// импорты single-export
 greetingFn(myName);
 
+// импорты export-and-import
 console.log(myOtherName);
 console.log(myFriendsname);
+console.log(myGreatHobbies);
