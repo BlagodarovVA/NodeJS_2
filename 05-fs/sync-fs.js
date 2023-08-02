73e6ee829c872c75085595e96fbba3baf192af05
Valery <11w-walerik@mail.ru>
2023-08-02 17:50:50 +0300

    76 done

--- 05-fs/sync-fs.js
+++ 05-fs/sync-fs.js
@@ -1,1 +1,13 @@
 const fs = require('fs');
+
+// синхронные методы, блокируют цикл
+try {
+	fs.writeFileSync('./first.txt', 'First file text');
+	console.log('File first.txt was written');
+	fs.appendFileSync('./first.txt', '\nOne more line');
+	console.log('Appended text to the file');
+	fs.renameSync('./first.txt', './renamed-first.txt');
+	console.log('File was renamed');
+} catch (error) {
+	console.log(error);
+}
