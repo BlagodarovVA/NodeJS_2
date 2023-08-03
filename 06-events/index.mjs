e2c69ba65393e3c26b19f759087bc290ad60d51d
Valery <11w-walerik@mail.ru>
2023-08-03 15:37:51 +0300

    81 done

--- 06-events/index.mjs
+++ 06-events/index.mjs
@@ -2,9 +2,8 @@ import { EventEmitter } from 'events';
 
 const myEmitter = new EventEmitter();
 
-myEmitter.on('timeout', (secondsQty) => {
+const timeoutListenerFn = (secondsQty) => {
 	console.log(`Timeout event in ${secondsQty} seconds.
-.
 ⠄⠄⣀⣤⣴⣾⣿⣷⣭⣭⣭⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠄⠄
 ⠄⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣸⣿⣿⣧⠄⠄
 ⠄⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⢻⣿⣿⡄⠄
@@ -14,7 +13,21 @@ myEmitter.on('timeout', (secondsQty) => {
 ⠄⠄⢸⣿⣮⡻⠿⣿⠿⣟⣫⣾⣿⣿⣿⣷⣶⣾⣿⡏⣿⣿⣿⡇
 ⠄⠄⢸⣿⣿⣿⡇⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⣿⣿⣿⡇
 `);
-});
+};
+
+myEmitter.on('timeout', timeoutListenerFn);
 
 setTimeout(() => myEmitter.emit('timeout', 1), 1000);
 setTimeout(() => myEmitter.emit('timeout', 2), 2000);
+
+myEmitter.once('singleEvent', () => {
+	console.log('Single event occured');
+});
+
+// Вызовется 1 раз
+setTimeout(() => myEmitter.emit('singleEvent'), 500);
+setTimeout(() => myEmitter.emit('singleEvent'), 1500);
+
+// Удаление листенера события timeout
+setTimeout(() => myEmitter.off('timeout', timeoutListenerFn), 3000);
+setTimeout(() => myEmitter.emit('timeout', 4), 4000);
