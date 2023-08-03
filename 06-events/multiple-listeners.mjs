ba08044ccb3a0ed698a46c33f61a3ccfecb1a881
Valery <11w-walerik@mail.ru>
2023-08-03 17:25:24 +0300

    82 done

--- 06-events/multiple-listeners.mjs
+++ 06-events/multiple-listeners.mjs
@@ -1,19 +1,19 @@
-import EventEmitter from 'events';
+import { EventEmitter } from 'events';
 
 const myEmitter = new EventEmitter();
 
 myEmitter.on('myEvent', () => {
-    console.log('First event listener');
+	console.log('First event listener');
 });
 
 myEmitter.on('myEvent', () => {
-    console.log('Second event listener');
+	console.log('Second event listener');
 });
 
 myEmitter.on('otherEvent', () => console.log('Other event'));
 
-// Default listeners qty is 10
-myEmitter.setMaxListeners(25);
+// 10 по умолчанию. на практике обычно 1-2
+myEmitter.setMaxListeners(20);
 console.log(myEmitter.getMaxListeners());
 
 console.log(myEmitter.eventNames());
