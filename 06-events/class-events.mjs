ddefe5b216e6e3dbb4375ad17a93290258316624
Valery <11w-walerik@mail.ru>
2023-08-04 15:13:00 +0300

    84 done

--- 06-events/class-events.mjs
+++ 06-events/class-events.mjs
@@ -1,37 +1,29 @@
 import EventEmitter from 'events';
 
 class Post extends EventEmitter {
-    constructor(author, text) {
-        super();
-        this.author = author;
-        this.text = text;
-        this.likesQty = 0;
-        this.on('likePost', (username) => {
-            console.log(`${username} liked your post!`);
-        });
-        this.on('error', (error) => {
-            console.error(error);
-        });
-    }
+	constructor(author, text) {
+		super();
+		this.author = author;
+		this.text = text;
+		this.likesQty = 0;
+	}
 
-    like(username) {
-        if (!username) {
-            return this.emit(
-                'error',
-                new Error('No username in the like request!')
-            );
-        }
-        this.likesQty += 1;
-        this.emit('likePost', username);
-    }
+	like(username) {
+		this.likesQty += 1;
+		this.emit('likePost', username);
+	}
 }
 
-const myPost = new Post('Bogdan', 'My great post!');
+const myPost = new Post('Valery', 'Перый пост');
+myPost.on('likePost', (username) => {
+	console.log(`Новый лайк от ${username}`);
+});
 
 // console.log(myPost.author);
 // console.log(myPost.text);
 // console.log(myPost.likesQty);
-myPost.like('alice');
-setTimeout(() => myPost.like(), 1000);
-setTimeout(() => myPost.like('alex'), 2000);
+
+myPost.like('Василий');
+setTimeout(() => myPost.like('Фросий'), 1000);
+setTimeout(() => myPost.like('Зинаида Павловна'), 2000);
 // console.log(myPost.likesQty);
