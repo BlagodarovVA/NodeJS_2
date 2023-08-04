250f9bfd45c7c83f3862d2d3895d940419862d7a
Valery <11w-walerik@mail.ru>
2023-08-04 15:45:16 +0300

    85 done

--- 06-events/class-events.mjs
+++ 06-events/class-events.mjs
@@ -6,24 +6,31 @@ class Post extends EventEmitter {
 		this.author = author;
 		this.text = text;
 		this.likesQty = 0;
+		this.on('likePost', (username) => {
+			console.log(`Новый лайк от ${username}`);
+		});
+		this.on('error', (error) => {
+			console.error(error);
+		});
 	}
 
 	like(username) {
-		this.likesQty += 1;
-		this.emit('likePost', username);
+		if (!username) {
+			this.emit('error', new Error('Введите имя пользователя для like'));
+		} else {
+			this.likesQty += 1;
+			this.emit('likePost', username);
+		}
 	}
 }
 
 const myPost = new Post('Valery', 'Перый пост');
-myPost.on('likePost', (username) => {
-	console.log(`Новый лайк от ${username}`);
-});
 
 // console.log(myPost.author);
 // console.log(myPost.text);
 // console.log(myPost.likesQty);
 
 myPost.like('Василий');
-setTimeout(() => myPost.like('Фросий'), 1000);
+setTimeout(() => myPost.like(), 1000);
 setTimeout(() => myPost.like('Зинаида Павловна'), 2000);
 // console.log(myPost.likesQty);
