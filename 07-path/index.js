0b55620cbc2218b2f7096735ba3c37a2dec707eb
Valery <11w-walerik@mail.ru>
2023-08-07 13:46:44 +0300

    88 done

--- 07-path/index.js
+++ 07-path/index.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
-const filePath = 'd:/JS/Node_2/07-path/index.js';
-const textFilePath = 'd:/JS/Node_2/07-path/file.txt';
-const relativePath = '../07-path/movie.mov';
-const directoryPath = '../07-path/subfolder';
+const filePath = 'd:\\JS\\Node_2\\07-path\\index.js';
+const textFilePath = 'd:\\JS\\Node_2\\07-path\\file.txt';
+const relativePath = '../07-path\\movie.mov';
+const directoryPath = '../07-path\\subfolder';
 
 console.log(path.isAbsolute(filePath)); // true
 console.log(path.isAbsolute(relativePath)); // false
@@ -15,3 +15,14 @@ console.log(path.dirname(filePath)); // d:/JS/Node_2/07-path
 console.log(path.dirname(directoryPath)); // ../07-path
 
 console.log(path.resolve(relativePath)); // d:\JS\07-path\movie.mov
+
+console.log(path.extname(textFilePath)); // .txt
+console.log(path.extname(directoryPath));
+
+console.log(path.parse(filePath));
+
+const parsedPath = path.parse(filePath);
+console.log(filePath);
+// d:\JS\Node_2\07-path\index.js
+console.log(path.join(parsedPath.dir, `renamed-${parsedPath.name}.mjs`));
+// d:\JS\Node_2\07-path\renamed-index.mjs
