95ef851823f70a080d2d042a61df172f78ff69b1
Valery <11w-walerik@mail.ru>
2023-08-08 12:00:18 +0300

    92 done

--- 08-http/index.js
+++ 08-http/index.js
@@ -1,12 +1,37 @@
 const http = require('http');
 
 const PORT = 5000;
+let count = 0;
+
+const comments = [
+	{ id: 100, text: 'First comment', author: 'Valery' },
+	{ id: 202, text: 'Second comment', author: 'Diana' },
+	{ id: 333, text: 'Last comment', author: 'Vasil' },
+];
 
 const server = http.createServer((req, res) => {
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/html');
-	res.end('<h1>Greating from the HTTP server</h1>');
-	console.log('Пользователь подключился'); // 2 раза принтует почему-то
+	if (req.url === '/html') {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/html');
+		res.write('<html><body><div>');
+		res.write('<h1>Greating from the HTTP server</h1>');
+		res.write('</div></body></html>');
+		count++;
+		console.log(`Запрос № ${count}: на /html`);
+		return res.end();
+	} else if (req.url === '/text') {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/html');
+		count++;
+		console.log(`Запрос № ${count}: на /text`);
+		return res.end('This is plain text');
+	} else if (req.url === '/json') {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		count++;
+		console.log(`Запрос № ${count}: на \/json`);
+		return res.end(JSON.stringify(comments));
+	}
 });
 
 server.listen(PORT, () => {
