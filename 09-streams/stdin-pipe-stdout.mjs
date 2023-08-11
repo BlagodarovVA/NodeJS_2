58c5ac55598e3eb9046c2c9665f2d24d25302fa6
Valery <11w-walerik@mail.ru>
2023-08-11 16:06:00 +0300

    108 done

--- 09-streams/stdin-pipe-stdout.mjs
+++ 09-streams/stdin-pipe-stdout.mjs
@@ -2,27 +2,19 @@ import { Transform } from 'stream';
 import fs from 'fs';
 
 const upperCaseStream = new Transform({
-    transform: function (chunk, encoding, cb) {
-        const upperCased = chunk.toString().toUpperCase();
-        cb(null, upperCased);
-    },
+	transform: function (chunk, encoding, cb) {
+		const upperCased = chunk.toString().toUpperCase();
+		console.log(upperCased);
+		cb(null, upperCased);
+	},
 });
 
-const reverseStream = new Transform({
-    transform(chunk, encoding, cb) {
-        const arrayOfChars = chunk.toString().split('');
-        const lastChar = arrayOfChars.pop();
-        const reversed = arrayOfChars.reverse().concat(lastChar).join('');
-        cb(null, reversed);
-    },
-});
+process.stdin.pipe(upperCaseStream).pipe(process.stdout);
 
-process.stdin.pipe(upperCaseStream).pipe(reverseStream).pipe(process.stdout);
-
-// // Pipe to file
+// // пайп в файл
 // const filePath = './files/stdin-dump.txt';
 // const writeStream = fs.createWriteStream(filePath);
 // process.stdin.pipe(writeStream);
 
-// // Pipe to stdout
-// process.stdin.pipe(process.stdout)
+// // пайп в stdout
+// process.stdin.pipe(process.stdout);
