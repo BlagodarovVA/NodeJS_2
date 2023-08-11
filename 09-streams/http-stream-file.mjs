dd0aaa8a97b0eebdd998661f8c9d293d0b97e9d9
Valery <11w-walerik@mail.ru>
2023-08-11 15:09:46 +0300

    106 done

--- 09-streams/http-stream-file.mjs
+++ 09-streams/http-stream-file.mjs
@@ -1,30 +1,31 @@
 import http from 'http';
 import fs from 'fs';
 
+const PORT = 5001;
+
 const server = http.createServer((req, res) => {
-    const filePath = './files/index.html';
-    // With streams
-    if (req.url === '/' && req.method === 'GET') {
-        const readStream = fs.createReadStream(filePath);
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        readStream.pipe(res);
-    }
-    // Without streams. We read entire file and then send it to the client
-    if (req.url === '/no-stream' && req.method === 'GET') {
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
-                res.statusCode = 500;
-                res.end('Error reading file on server');
-            } else {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'text/html');
-                res.end(data);
-            }
-        });
-    }
+	const filePath = './files/index.html';
+	if ((req.url === '/') & (req.method === 'GET')) {
+		const readStream = fs.createReadStream(filePath);
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/html');
+		readStream.pipe(res);
+	}
+	if ((req.url === '/no-stream') & (req.method === 'GET')) {
+		fs.readFile(filePath, (err, data) => {
+			if (err) {
+				res.statusCode = 500;
+				console.log('no-stream: ', err);
+				res.end('Error reading file on server');
+			} else {
+				res.statusCode = 200;
+				res.setHeader('Content-Type', 'text/html');
+				res.end(data);
+			}
+		});
+	}
 });
 
-server.listen(5000, () => {
-    console.log('Server is listening at port 5000');
+server.listen(PORT, () => {
+	console.log(`Сервер слушается на порту ${PORT}`);
 });
