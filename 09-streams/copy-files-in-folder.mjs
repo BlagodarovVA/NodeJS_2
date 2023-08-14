47466262f52d2069556bc553dde70ed09d84be9d
Valery <11w-walerik@mail.ru>
2023-08-14 17:23:13 +0300

    116 done

--- 09-streams/copy-files-in-folder.mjs
+++ 09-streams/copy-files-in-folder.mjs
@@ -11,9 +11,38 @@ if (!fs.existsSync(sourceDir)) {
 }
 
 if (fs.existsSync(destinationDir)) {
-	fs.rmdirSync(destinationDir);
+	// уудаляем папку и содержимое
+	fs.rmSync(destinationDir, { recursive: true });
 	console.log(`Папка ${destinationDir} удалена`);
 }
 
 fs.mkdirSync(destinationDir);
 console.log(`Папка ${destinationDir} создана`);
+
+fs.readdir(sourceDir, (err, fileNames) => {
+	try {
+		if (err) {
+			console.log(err);
+			process.exit(1);
+		}
+
+		fileNames.forEach((fileName, index) => {
+			const sourceFilePath = path.join(sourceDir, fileName);
+			const destFilePath = path.join(
+				destinationDir,
+				`${index + 1}. ${fileName}`
+			);
+
+			const readFileStream = fs.createReadStream(sourceFilePath);
+			const writeFileStream = fs.createWriteStream(destFilePath);
+
+			readFileStream.pipe(writeFileStream);
+
+			writeFileStream.on('finish', () => {
+				console.log(`Файл ${fileName} скопирован`);
+			});
+		});
+	} catch (error) {
+		console.log('Ошибка try: ', error);
+	}
+});
