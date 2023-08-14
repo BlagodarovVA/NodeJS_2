7394951fc7d694e17568d440e35f43bfeb4cd8b1
Valery <11w-walerik@mail.ru>
2023-08-14 16:04:00 +0300

    115 done

--- 09-streams/copy-file-in-stream.mjs
+++ 09-streams/copy-file-in-stream.mjs
@@ -1,13 +1,13 @@
 import fs from 'fs';
 
-const fileName = './files/fifth.txt';
-const copiedFileName = './files/fifth-copy.txt';
+const fileName = './files/file.log';
+const copiedFileName = './copied-files/file.log';
 
 const readStream = fs.createReadStream(fileName);
 const writeStream = fs.createWriteStream(copiedFileName);
 
 readStream.pipe(writeStream);
 
-readStream.on('end', () => console.log('Read stream ended'));
-writeStream.on('finish', () => console.log('File was copied'));
-writeStream.on('close', () => console.log('Write stream closed'));
+readStream.on('end', () => console.log('INFO: Поток чтения завершен'));
+writeStream.on('finish', () => console.log('INFO: Файл скопирован'));
+writeStream.on('close', () => console.log('INFO: Поток записи завершен'));
--- 09-streams/copy-files-in-folder.mjs
+++ 09-streams/copy-files-in-folder.mjs
@@ -5,37 +5,15 @@ const sourceDir = './files';
 const destinationDir = './copied-files';
 
 if (!fs.existsSync(sourceDir)) {
-    console.warn(`Source dir ${sourceDir} doesn't exist!`);
-    console.log('Exiting...');
-    process.exit(0);
+	console.warn(`Исходная папка ${sourceDir} не существует`);
+	console.log('Exiting...');
+	process.exit(0);
 }
 
 if (fs.existsSync(destinationDir)) {
-    fs.rmSync(destinationDir, { recursive: true });
+	fs.rmdirSync(destinationDir);
+	console.log(`Папка ${destinationDir} удалена`);
 }
+
 fs.mkdirSync(destinationDir);
-
-fs.readdir(sourceDir, (err, fileNames) => {
-    if (err) {
-        console.log(err);
-        process.exit(1);
-    }
-    console.log('Start', performance.now());
-    fileNames.forEach((fileName, index) => {
-        const sourceFilePath = path.join(sourceDir, fileName);
-        const destinationFilePath = path.join(
-            destinationDir,
-            `${index + 1}. ${fileName}`
-        );
-
-        const readFileStream = fs.createReadStream(sourceFilePath);
-        const writeFileStream = fs.createWriteStream(destinationFilePath);
-
-        readFileStream.pipe(writeFileStream);
-
-        writeFileStream.on('finish', () => {
-            console.log(`File ${fileName} was copied`);
-        });
-    });
-    console.log('End', performance.now());
-});
+console.log(`Папка ${destinationDir} создана`);
