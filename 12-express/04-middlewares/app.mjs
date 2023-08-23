b0dec6c2365f914f13108d0717f4539184dc6d11
Valery Blagodarov <11w-walerik@mail.ru>
2023-08-23 17:05:31 +0300

    162 done

--- 12-express/04-middlewares/app.mjs
+++ 12-express/04-middlewares/app.mjs
@@ -1,20 +1,25 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 const PORT = 5001;
 
-const logger = (req, res, next) => {
-	console.log(req.method, req.path);
-	next();
-};
+app.use(morgan('dev'));
 
-app.use(logger);
+app.use(express.json()); // метод делает то же, что и строки 10-18
+// app.use((req, res, next) => {
+// 	let data = '';
+// 	req.on('data', (chunk) => (data += chunk));
+// 	req.on('end', () => {
+// 		const parsedJSON = JSON.parse(data);
+// 		req.body = parsedJSON;
+// 		next();
+// 	});
+// });
 
-app.use((req, res) =>
-	res.send(`
-<h1>Привет</h1>
-<h1>Это express сервер...</h1>
-`)
-);
+app.use((req, res) => {
+	console.log(req.body);
+	return res.send(`<h1>Это express сервер...</h1>`);
+});
 
 app.listen(PORT, () => console.log(`Сервер слушает на порту ${PORT}`));
